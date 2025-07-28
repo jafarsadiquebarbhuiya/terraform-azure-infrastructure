987c4d46d6e81f9b52935abe2fbaa2874e078624
Jafar Sadique Barbhuiya <sadiquejafar25@gmail.com>
2025-07-28 14:56:11 +0000

    corrected output error

--- modules/resourcegroup/outputs.tf
+++ modules/resourcegroup/outputs.tf
@@ -1,8 +1,9 @@
 output "resource_group_name_output" {
-  description = "Resource group name"
-  value       = azurerm_resource_group.dev_rg.name
+  description = "Names of all created resource groups"
+  value       = azurerm_resource_group.dev_rg[*].name
 }
+
 output "resource_group_name_id" {
-  description = "Resource group ID"
-  value       = azurerm_resource_group.dev_rg.id
+  description = "IDs of all created resource groups"
+  value       = azurerm_resource_group.dev_rg[*].id
 }
